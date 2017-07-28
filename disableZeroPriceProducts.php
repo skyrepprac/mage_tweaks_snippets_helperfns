8163d97dd7a3e83638bd4bc6bebaa877d54aba31
skyrepprac <viveks@e2msol.com>
2017-07-28 19:04:14 +0530

    Add files via upload

---
+++ disableZeroPriceProducts.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+set_time_limit(0);
+ini_set('memory_limit','1024M');
+require_once __DIR__ . '/app/Mage.php';
+umask(0);
+Mage::app('default');
+
+$zeroPriceProducts = Mage::getModel('catalog/product')->getCollection()
+						->addAttributeToFilter(
+							array(
+								array('attribute'=>'price', 'eq'=>'0'),
+								array('attribute'=>'price', 'isnull'=>true),
+							)
+						);
+$ids = $zeroPriceProducts->getAllIds();
+try {
+	Mage::getSingleton('catalog/product_action')->updateAttributes(
+	    $ids,
+	    array('status' => 2),
+	    0
+	);
+	echo "All products with 0 price have been disabled !!!";
+} catch(Exception $e) {
+	echo "Error occurred disabling the products: ".$e;
+	exit(0);
+}
