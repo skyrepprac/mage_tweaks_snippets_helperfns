9b9a5607077966665bb630a9160930cfa1354bef
skyrepprac <viveks@e2msol.com>
2017-08-01 02:18:35 +0530

    Update Mage_Extended_Helper_Core_Functions.php

--- Mage_Extended_Helper_Core_Functions.php
+++ Mage_Extended_Helper_Core_Functions.php
@@ -203,4 +203,15 @@ class Mage_Extended_Helper_Core_Functions extends Mage_Core_Helper_Data
 			}
 		}
 	}
+	
+	public function randPassGen($length = 12) {
+		$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+		$pass = array(); //remember to declare $pass as an array
+		$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+		for ($i = 0; $i < $length; $i++) {
+		    $n = rand(0, $alphaLength);
+		    $pass[] = $alphabet[$n];
+		}
+		return implode($pass); //turn the array into a string
+	}
 }
